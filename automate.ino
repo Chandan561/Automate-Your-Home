096c12df65174c77998bb3f308074c3490c51682
Chandan561 <30626257+Chandan561@users.noreply.github.com>
2017-08-01 18:56:42 +0530

    Add files via upload
    Arduino code for controlling of appliances using relay module with android phone.

---
+++ automate.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(10, 11); 
+String readcommand;
+
+void setup() {
+ BT.begin(9600);
+ Serial.begin(9600);
+  pinMode(8, OUTPUT);
+
+}
+void loop() {
+  while (BT.available()){  
+  delay(10); 
+  char c = BT.read(); 
+  readcommand += c; 
+  } 
+  if (readcommand.length() > 0) {
+    Serial.println(readcommand);
+
+  if(readcommand == "ON")
+  {
+    digitalWrite(8,HIGH);
+    delay(100);
+  }
+
+  else if(readcommand == "OFF")
+  {
+    digitalWrite(8, LOW);
+    delay(100);
+  }
+
+  
+
+
+readcommand="";}} 
